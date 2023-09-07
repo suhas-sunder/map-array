6bf5db8a6f42044b3c4e7ae3c8bf1b7149a517fc
suhas-sunder <77464593+suhas-sunder@users.noreply.github.com>
2023-09-06 21:20:25 -0400

    feat: input validation
    -Add input validation for array to check all comma separated values are numeric. -Add input validation for equation to check user has entered a number.

--- index.js
+++ index.js
@@ -4,15 +4,36 @@ const errElement = document.getElementById("err");
 const getInput = () => {
   const arrInput = inputElement[0].value;
   const eqnInput = inputElement[1].value;
-  console.log(`Array: ${arrInput}`);
-  console.log(`Equation: ${eqnInput}`);
+
+  // Display error msg
+  const errorMessage = (msg) => {
+    errElement.textContent = msg;
+  };
 
   if (arrInput && eqnInput) {
-    // Check if Array value is valid
+    // Check if Array value is valid & map numbers to a new array.
+    const numsArr = [];
+
+    // Check if each comma separated value is a valid number
+    arrInput
+      .split(",")
+      .forEach((char) =>
+        !isNaN(char.trim())
+          ? numsArr.push(parseInt(char))
+          : errorMessage(
+              "Error: please enter a valid comma separated array of numbers eg. 1, 2, 3, 4!"
+            )
+      );
+
+    console.log(numsArr.length, arrInput.split(",").length);
+
     //If input is not x, display number input.
+    !isNaN(eqnInput.trim())
+      ? console.log(eqnInput.trim())
+      : errorMessage("Please enter a valid number for the selected equation!");
     // Hide error message unless it needs to be displayed.
     //Add some CSS styling.
   } else {
-    errElement.textContent = "Error: Input fields cannot be empty!";
+    errorMessage("Error: Input fields cannot be empty!");
   }
 };
