b9d1c3835667fa0a18fdd86346c830370376a8ec
suhas-sunder <77464593+suhas-sunder@users.noreply.github.com>
2023-09-07 23:56:36 -0400

    feat: handle selction
    -Add listener to select option so that number input is only displayed when an equation is selected. -Add a switch statement to handle map operations when submitted.

--- index.js
+++ index.js
@@ -1,16 +1,63 @@
 const inputElement = document.getElementsByClassName("inputs");
 const errElement = document.getElementById("err");
 
+const getSelectedEqn = () => {
+  // Retrieve selected value from drop-down list
+  const selectedEquation = document.getElementById("equation");
+  const index = selectedEquation.selectedIndex;
+  const equationValue = selectedEquation.options[index].text;
+  return equationValue;
+};
+
+const handleSelection = (e) => {
+  const numInputElement = document.getElementById("num");
+  console.log(getSelectedEqn())
+
+  getSelectedEqn() === "x"
+    ? (numInputElement.style.display = "none")
+    : (numInputElement.style.display = "inline-block");
+};
+
 const getInput = () => {
   const arrInput = inputElement[0].value;
   const eqnInput = inputElement[1].value;
 
+  const eqn = getSelectedEqn();
+
+  switch (eqn) {
+    case 0:
+      equationValue.includes("*");
+      // Perform map
+      break;
+    case 1:
+      equationValue.includes("+");
+      // Perform map
+      break;
+    case 2:
+      equationValue.includes("-");
+      // Perform map
+      break;
+    case 3:
+      equationValue.includes("/");
+      // Perform map
+      break;
+    case 4:
+      equationValue.includes("^");
+      // Perform map
+      break;
+    default:
+    // No input selected.
+  }
+
   // Display error msg
   const errorMessage = (msg) => {
+    errElement.style.display = "flex";
     errElement.textContent = msg;
   };
 
   if (arrInput && eqnInput) {
+    // Hide error message
+    errElement.style.display = "none";
     // Check if Array value is valid & map numbers to a new array.
     const numsArr = [];
 
@@ -31,7 +78,9 @@ const getInput = () => {
     !isNaN(eqnInput.trim())
       ? console.log(eqnInput.trim())
       : errorMessage("Please enter a valid number for the selected equation!");
-    // Hide error message unless it needs to be displayed.
+
+    //Hide equation if option is just x
+    //Implement map operation
     //Add some CSS styling.
   } else {
     errorMessage("Error: Input fields cannot be empty!");
